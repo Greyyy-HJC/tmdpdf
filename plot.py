2333eac99978c3e20195be4b69cb2d5a536c4b8b
Greyyy-HJC <hejinchen17@gmail.com>
2023-04-07 15:47:36 -0400

    gsfit_done

--- plot.py
+++ plot.py
@@ -7,9 +7,11 @@ from funcs import *
 
 t_label = r'$\rm{t (a) }$'
 meff_label = r'$m_{eff}$'
+chi2_label = r'$\chi^2 / d.o.f.$'
+Q_label = r'$Q$'
 
 
-def meff_plot(pt2_ls, ti, tf, fit_res, mom_ls, mom_plot, title):
+def meff_plot(pt2_ls, ti, tf, fit_res, title):
     meff_ls = pt2_to_meff(pt2_ls)
 
     fig = plt.figure(figsize=fig_size)
@@ -175,3 +177,45 @@ def fit_on_data_plot_ratio(ra_t, ra_tau, ra_re_gv, ra_im_gv, fit_res, title, log
     plt.ylabel(r'g.s.', font)
     plt.savefig(log_folder+title+'_imag.pdf', transparent=True)
     plt.show()
+
+'''
+define a function to make a histogram plot of a list of values.
+'''
+def hist_plot(ls, xlabel, title, xlim=None, save=True):
+    fig = plt.figure(figsize=fig_size)
+    ax = plt.axes(plt_axes)
+
+    ax.hist(ls, bins=40, density=True, rwidth=1, alpha=0.4, edgecolor='black', linewidth=0.5)
+
+    ax.tick_params(direction='in', top='on', right='on', **ls_p)
+    ax.grid(linestyle=':')
+    ax.set_xlabel(xlabel, font)
+    ax.set_ylabel(r'Density', font)
+    plt.title(title, font)
+    plt.xlim(xlim)
+    if save == True:
+        plt.savefig('fig/'+title+'.pdf', transparent=True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    import gvar as gv
+    import os
+
+    #* iterate over all files in the folder
+    collect_chi2 = []
+    collect_Q = []
+
+    for file in os.listdir('dump/gs_fit'):
+        collect_chi2.append(gv.load('dump/gs_fit/'+file)['chi2'])
+        collect_Q.append(gv.load('dump/gs_fit/'+file)['Q'])
+
+
+    collect_chi2_flat = np.array(collect_chi2).reshape(-1)
+    collect_Q_flat = np.array(collect_Q).reshape(-1)
+    
+    hist_plot(collect_chi2_flat, chi2_label, 'chi2_distribution_hist', xlim=(0, 3), save=True)
+
+    # hist_plot(collect_Q_flat, Q_label, 'Q_distribution_hist', xlim=(0, 1), save=True)
+
+# %%
